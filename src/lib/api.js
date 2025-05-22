036311ebbf27c4bb65968f96bc4b2d5d05182650
Fabrian28 <fabrianrafi@outlook.com>
2025-05-22 07:52:42 +0700

    feat: update back-end url

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://gdwcknhx-3000.asse.devtunnels.ms/api",
+    baseURL: "https://bhgkx5m5-3000.asse.devtunnels.ms/api",
 });
 
 export default api;
